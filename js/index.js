f53b5faee64f7bb28daeb8d821eb26fdf2cc5792
Michael <mike.l_42@hotmail.com>
2017-12-02 16:42:02 -0500

    test4

--- js/index.js
+++ js/index.js
@@ -30,6 +30,5 @@ async function firebaseSaveData() {
   });
 
   document.getElementById("upload_snapshot").click();
-  await sleep(1200);
-  // document.location.href = "thanks/thankyou.html";
+
 }
